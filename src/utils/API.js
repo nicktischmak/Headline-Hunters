b125abd6dcdb9b33e97e292f3248c928914bf6b5
nicktischmak <nicktischmak@gmail.com>
2024-11-30 17:48:05 -0600

    updated function

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,9 @@
 import fetch from 'node-fetch';
 
-const search = async (query) =>
-  fetch.get(`https://newsapi.org/v2/everything?q=${query}&from=2024-11-26&to=2024-11-26&sortBy=popularity&apiKey=76acae67181d47ca93388145e8c95b85`);
+const search = async (query) => {
+  const response = await fetch(`https://newsapi.org/v2/everything?q=${query}&from=2024-11-26&to=2024-11-26&sortBy=popularity&apiKey=76acae67181d47ca93388145e8c95b85`);
+  const data = await response.json();
+  return data;
+};
 
 export default { search };
