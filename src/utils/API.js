18a084864932633ff57c60d5c13e6b7e376abede
nicktischmak <nicktischmak@gmail.com>
2024-11-30 15:00:04 -0600

    changed API search function

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 const search = async (query) =>
-  axios.get(`https://newsapi.org/v2/everything?q=apple&from=2024-11-26&to=2024-11-26&sortBy=popularity&apiKey=76acae67181d47ca93388145e8c95b85`);
+  axios.get(`https://newsapi.org/v2/everything?q=${query}&from=2024-11-26&to=2024-11-26&sortBy=popularity&apiKey=76acae67181d47ca93388145e8c95b85`);
 
-export default search;
+export default { search };
